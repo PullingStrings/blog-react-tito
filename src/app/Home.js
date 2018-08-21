6af559b63c29558975d1955b21ef1fe2cda4461e
Tito Zwane <drammatik@gmail.com>
2018-08-21 17:19:05 +0100

    styling components

--- src/app/Home.js
+++ src/app/Home.js
@@ -1,21 +1,45 @@
 import React from 'react';
+import styled from 'styled-components';
 
+const divOne = styled.div`
+  background-color: #000;
+  position: relative;
+  z-index: 1;
+`;
+
+console.log(divOne);
+
+const divTwo = `
+  background-color: blue;
+  position: absolute;
+  z-index: auto;
+`;
 
 const Home = () => (
   <div>
-    <div className="container is-fullhd">
-      <img src={require('./assets/motorbike.png')} alt='motorbike' />
+    <div className="columns">
+      <div className="column"
+        style={{ divOne }}>
+        <div className="container is-fullhd">
+          <img src={require('./assets/motorbike.png')} alt='motorbike' />
+        </div>
+        <div className="container is-fullhd">
+          <img src={require('./assets/specking.jpg')}
+            style={{ width: '100%' }}
+            alt='public-specking' />
+        </div>
+        <div className="container is-fullhd">
+          <img src={require('./assets/performing.jpg')}
+            style={{ width: '100%' }}
+            alt='performing' />
+        </div>
+      </div>
+      <div className="column"
+        style={{ divTwo }}>
+        div 2
+      </div>
     </div>
-    <div className="container is-fullhd">
-      <img src={require('./assets/specking.jpg')}
-        style={{ width: '100%' }}
-        alt='public-specking' />
-    </div>
-    <div className="container is-fullhd">
-      <img src={require('./assets/performing.jpg')}
-        style={{ width: '100%' }}
-        alt='performing' />
-    </div>
+
   </div>
 );
 export default Home;
