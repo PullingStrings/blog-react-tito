1490ab66d18759e07fe5bac0722ab6253058feaf
Tito Zwane <drammatik@gmail.com>
2018-08-28 14:34:26 +0100

    setup for redux

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
+//Redux Store
+import { Provider } from 'react-redux';
+import { configureStore } from './src/store';
+
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-
+const store = configureStore();
 
 ReactDOM.render((
-  <Router>
-    <App />
-  </Router>
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>
 ), document.getElementById('root'));
 registerServiceWorker();
