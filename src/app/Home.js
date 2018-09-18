b49b4da2e7b0a7c62b9511ed274253cd6be407e5
Tito Zwane <drammatik@gmail.com>
2018-09-18 10:13:06 +0100

    responsize

--- src/app/Home.js
+++ src/app/Home.js
@@ -15,34 +15,24 @@ const headerType = {
 };
 
 const Home = () => (
-
-
   <div>
     <div className="container is-fullhd">
-      {/* <img src={require('./assets/motorbike.png')} alt="motorbike" />
-      <img src={require('./assets/specking.jpg')} alt="specking" />
-      <img src={require('./assets/performing.jpg')} alt="performing"/>
-      <img src={require('./assets/laptop.jpg')} alt="sitting down" /> */}
       <div className="columns">
         <div className="column" >
           <Typing speed={100}>
             <div style={headerType}>
-              <h1>Web developer...</h1>
-
-              <h1>Art Lover...</h1>
-
-              <h1>Music Player...</h1>
-
-              <h1>Blogger</h1>
+              <h1>Web developer</h1>
+              <h1>Art Lover</h1>
+              <h1>Music Player</h1>
+              <h1>Blogger...</h1>
             </div>
           </Typing>
-          <br/>
           <p className="mbr-author-name pt-4 mb-2 mbr-fonts-style display-7">Sibusiso Zwane.</p>
           <p className="mbr-author-desc mbr-fonts-style display-7">Web Developer<br/>
             <a href="https://github.com/PullingStrings" className="text-danger">https://github.com/PullingStrings</a><br/><a href="https://www.linkedin.com/in/titozwane/" className="text-secondary">https://www.linkedin.com/in/titozwane/</a><br/><a href="mailto:titozwane28@gmail.com" className="text-success">titozwane28@gmail.com</a></p>
         </div>
         <div className="column">
-          <div style={colOne}></div>
+          <img src={require('./assets/laptop.jpg')} alt="sitting down" />
         </div>
       </div>
     </div>
